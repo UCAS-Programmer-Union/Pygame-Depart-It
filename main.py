f1f52f60f351225f3d1b5e60e6c2e0c384163891
Sean Shin <seanshin2647@gmail.com>
2020-01-09 22:18:36 +0000

    Create the display

--- main.py
+++ main.py
@@ -12,3 +12,5 @@ for config in configs["main"]:
     FPS = config["FPS"]
     DISPLAY_WIDTH = config["DISPLAY_WIDTH"]
     DISPLAY_HEIGHT = config["DISPLAY_HEIGHT"]
+
+DISPLAY = pygame.display.set_mode([DISPLAY_WIDTH, DISPLAY_HEIGHT])
