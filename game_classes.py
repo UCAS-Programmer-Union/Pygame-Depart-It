8eb5db10aea72caff0fda0d0f703220b4557d1d4
Sean Shin <seanshin2647@gmail.com>
2020-01-13 22:23:17 +0000

    Create __init__ with arguments

--- game_classes.py
+++ game_classes.py
@@ -11,4 +11,6 @@ for config in configs:
     WALL_BLOCK_LENGTH = configs["WALL_BLOCK_LENGTH"]
 
 class Wall_Block(pygame.sprite.Sprite):
-    
+    def __init__(self, x_map_index, y_map_index, wall_block_width = WALL_BLOCK_WIDTH,
+        wall_block_length = WALL_BLOCK_LENGTH, color = clr.WHITE):
+        
