3728ec0bdba3f109ba43db4b24a3da56266e03a8
Sean Shin <seanshin2647@gmail.com>
2020-01-13 22:19:34 +0000

    Read in configs and assign to variables

--- game_classes.py
+++ game_classes.py
@@ -5,3 +5,8 @@ import color as clr
 
 with open("config.json") as config_json:
     configs = json.load(config_json)
+
+for config in configs:
+    WALL_BLOCK_WIDTH = configs["WALL_BLOCK_WIDTH"]
+    WALL_BLOCK_LENGTH = configs["WALL_BLOCK_LENGTH"]
+
