772c1a524daa38aee6046cf37675daa42f3f0fba
Sean Shin <seanshin2647@gmail.com>
2020-01-14 15:04:58 -0700

    Rename Wall_Block to follow PEP8

--- game_classes.py
+++ game_classes.py
@@ -10,7 +10,7 @@ for config in configs:
     WALL_BLOCK_WIDTH = configs["WALL_BLOCK_WIDTH"]
     WALL_BLOCK_LENGTH = configs["WALL_BLOCK_LENGTH"]
 
-class Wall_Block(pygame.sprite.Sprite):
+class WallBlock(pygame.sprite.Sprite):
     def __init__(self, x_map_index, y_map_index, wall_block_width = WALL_BLOCK_WIDTH,
         wall_block_length = WALL_BLOCK_LENGTH, color = clr.WHITE):
         super().__init__()
