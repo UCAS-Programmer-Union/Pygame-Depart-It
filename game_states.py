6a307b8d56bf3cd83fdde59a394598d749a70e76
Sean Shin <seanshin2647@gmail.com>
2020-02-05 22:01:35 +0000

    Create class State

--- game_states.py
+++ game_states.py
@@ -1,1 +1,12 @@
+class State():
+    def __init__():
+        pass
 
+    def render():
+        raise NotImplementedError
+
+    def update():
+        raise NotImplementedError
+
+    def handle_events():
+        raise NotImplementedError
