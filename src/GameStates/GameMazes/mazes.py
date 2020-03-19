c4809a3f3f12ccacdb1f329fde531f617237dd98
Sean Shin <seanshin2647@gmail.com>
2020-03-18 19:02:33 -0600

    Create function for converting raw mazes into list

--- src/GameStates/GameMazes/mazes.py
+++ src/GameStates/GameMazes/mazes.py
@@ -6,3 +6,16 @@ def open_maze(maze_number):
 
     return _convert_maze(raw_maze)
 
+def _convert_maze(raw_maze_string):
+    raw_maze_string = raw_maze_string.splitlines()
+    map_list = []
+    temp_map_list = []
+    # The 24 is for the resolution/block_width
+    for row in raw_maze_string:
+        for column in row:
+            temp_map_list.append(column)
+
+        map_list.append(temp_map_list)
+        temp_map_list = []
+
+    return map_list
